26197e1436bb37976db790d8799b9bce946b32e2
Amit Karn <amit.karn98@gmail.com>
2023-09-21 23:52:58 +0545

    fast api init commit

---
+++ app/routers/users.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, BackgroundTasks
+from ..tasks import write_notification
+router = APIRouter()
+
+
+@router.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@router.get("/users/me", tags=["users"])
+async def read_user_me():
+    return {"username": "fakecurrentuser"}
+
+
+@router.get("/users/{username}", tags=["users"])
+async def read_user(username: str):
+    return {"username": username}
+
+
+
+@router.post("/users/send-notification/{email}")
+async def send_notification(email: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_notification, email, message="some notification")
+    return {"message": "Notification sent in the background"}
+
+
